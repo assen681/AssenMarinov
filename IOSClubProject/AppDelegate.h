991c3c68a617d0c34a8eff2ff50ae472af6b7cfd
Chloe Cooper <chloecooper@chloes-macbook-pro.local>
2016-02-17 18:42:07 -0500

    Initial Commit

---
+++ IOSClubProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSClubProject
+//
+//  Created by Chloe Cooper on 2/17/16.
+//  Copyright © 2016 Chloe Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
