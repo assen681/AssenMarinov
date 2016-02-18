65bd1ae070b2dc36c87973eeb03f974f2f1009f0
Chloe Cooper <chloecooper@chloes-macbook-pro.local>
2016-02-17 20:03:15 -0500

    gg

---
+++ IOSClubProject/CounterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CounterViewController.h
+//  IOSClubProject
+//
+//  Created by Chloe Cooper on 2/17/16.
+//  Copyright © 2016 Chloe Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CounterViewController : UIViewController
+
+@end
---
+++ IOSClubProject/TotalCountViewController.m
@@ -0,0 +1,37 @@
+//
+//  TotalCountViewController.m
+//  IOSClubProject
+//
+//  Created by Chloe Cooper on 2/17/16.
+//  Copyright © 2016 Chloe Cooper. All rights reserved.
+//
+
+#import "TotalCountViewController.h"
+
+@interface TotalCountViewController ()
+
+@end
+
+@implementation TotalCountViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
