12ba6bdf59758f3268fdaf61b87255c4c583f722
Chloe Cooper <chloecooper@chloes-macbook-pro.local>
2016-02-22 19:05:34 -0500

    icon edit

--- IOSClubProject/CounterViewController.m
+++ IOSClubProject/CounterViewController.m
@@ -9,7 +9,15 @@
 #import "CounterViewController.h"
 
 @interface CounterViewController ()
+@property (nonatomic,weak) IBOutlet UILabel *timerLabel;
+@property (nonatomic,weak) IBOutlet UIButton *cancelButton;
+@property (nonatomic,weak) IBOutlet UIButton *startButton;
 
+@property (nonatomic, strong) NSTimer *timer;
+
+@property (nonatomic, assign) NSInteger defaultTime;
+@property (nonatomic, assign) NSInteger currentTimeRemaining;
+@property (nonatomic, assign) BOOL isTimerRunning;
 @end
 
 @implementation CounterViewController
@@ -17,21 +25,75 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.defaultTime=30*60;
+    self.currentTimeRemaining =self.defaultTime;
+    self.isTimerRunning = NO;
+    [self updateLabelWithTime];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(IBAction)startPressed{
+    if (self.isTimerRunning) {
+        [self pauseTimer];
+        [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
+    else{
+        [self pauseTimer];
+        [self.startButton setTitle:@"Pause" forState:UIControlStateNormal];
+    }
+    self.isTimerRunning = !self.isTimerRunning;
+}
+-(IBAction)cancelPressed{
+    self.isTimerRunning = NO;
+    [self cancelTimer];
+    [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+    
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void) timerExecuted{
+    self.currentTimeRemaining--;
+    if (self.currentTimeRemaining <0) {
+        [self pauseTimer];
+        self.currentTimeRemaining = 0;
+    }
+    
+    [self updateLabelWithTime];
 }
-*/
+
+-(void)startTimer{
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector (timerExecuted) userInfo:nil repeats:YES];
+}
+
+-(void)pauseTimer{
+    [self.timer invalidate];
+    self.timer = nil;
+}
+
+-(void)cancelTimer{
+    [self pauseTimer];
+    self.currentTimeRemaining = self.defaultTime;
+    [self updateLabelWithTime];
+}
+
+-(void)updateLabelWithTime{
+    NSInteger secondsRemaining;
+    NSInteger minutesRemaining;
+    
+    
+    minutesRemaining =self.currentTimeRemaining/60;
+    secondsRemaining =self.currentTimeRemaining%60;
+    NSString *secondString;
+    
+    if(secondsRemaining<10){
+        secondString = [NSString stringWithFormat: @"0%zd",secondsRemaining];
+    }
+    else{
+        secondString = [NSString stringWithFormat: @"%zd",secondsRemaining];
+    }
+    self.timerLabel.text = [NSString stringWithFormat:@"%zd:%@",minutesRemaining,secondString];
+}
+
 
 @end
